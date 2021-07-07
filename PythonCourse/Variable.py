f000c82ae057c53f8ec4d14b3a729ec8b097988a
Kasra Code <kasracodec@gmail.com>
2021-07-07 23:19:10 +0430

    Create Variable.py

---
+++ PythonCourse/Variable.py
@@ -0,0 +1,33 @@
+# Integer :
+age = 17
+# print(age)
+
+# String :
+name = "My name is Kasra"
+# print(name)
+
+# Boolean :
+lamp = True
+#print(lamp)
+
+# Float :
+Pnumber = 3.14
+#print(Pnumber)
+
+# ------------------
+
+# Camel case :
+myVar = 4
+
+# Pascal case :
+MyVar = "Hello Im Pascal case . "
+# Snake case :
+my_var_4_ep2 = 'Hello Im Snake case'
+# ------------------
+
+# type :
+print(type(age))
+print(type(name))
+print(type(Pnumber))
+print(type(lamp))
+# @KasraCode
